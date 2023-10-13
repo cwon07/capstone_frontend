0962b35362f8badf55ef2dacb694aff1f981cc83
Christine Wong <christine@Christines-iMac.local>
2023-10-13 15:58:51 -0700

    header created

--- wtpa/src/app/app.module.ts
+++ wtpa/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RsvpComponent } from './rsvp/rsvp.component';
 import { RsvpsComponent } from './rsvps/rsvps.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RsvpComponent,
-    RsvpsComponent
+    RsvpsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
