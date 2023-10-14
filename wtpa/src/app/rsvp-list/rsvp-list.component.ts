c9f93ea816804763bdc6f160831c3f28e0f94c6b
Christine Wong <christine@Christines-iMac.local>
2023-10-13 23:14:23 -0700

    created guest model and updated rsvp list

--- wtpa/src/app/rsvp-list/rsvp-list.component.ts
+++ wtpa/src/app/rsvp-list/rsvp-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Guest } from '../shared/guest.model';
+
 @Component({
   selector: 'app-rsvp-list',
   templateUrl: './rsvp-list.component.html',
   styleUrls: ['./rsvp-list.component.css']
 })
 export class RsvpListComponent {
-
+  guests: Guest[] = [
+    new Guest('Chris', 1, 2),
+    new Guest('Jemma', 2, 1)
+  ];
 }
